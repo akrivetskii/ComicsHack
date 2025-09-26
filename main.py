ecacb3f8de6bc0ad409805c4378f529c629474e5
akrivetskii <a.krivetskii@g.nsu.ru>
2025-09-26 17:15:04 +0700

    Add image

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ def generate():
     return response.text
 
 
+API_KEY = "AIzaSyD8pTi_OfJwLPK6F_fg_ePoMqfVjvu4QuM"
+
 app = FastAPI()
-client = genai.Client(api_key="AIzaSyD8pTi_OfJwLPK6F_fg_ePoMqfVjvu4QuM")
-model = "gemini-2.5-flash"
+client = genai.Client(api_key=API_KEY)
+model_chat = "gemini-2.5-flash"
+model_image = "models/imagen-4.0-generate-001"
 
 @app.get("/chat")
 def chat_endpoint(prompt: str = Query(..., description="Текст запроса к модели")):
@@ -40,11 +43,37 @@ def chat_endpoint(prompt: str = Query(..., description="Текст запрос�
                 parts=[types.Part.from_text(text=prompt)],
             ),
         ]
-        response = client.models.generate_content(model=model, contents=contents)
+        response = client.models.generate_content(model=model_chat, contents=contents)
         return {"response": response.text}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка генерации: {str(e)}")
+
+
+@app.get("/image")
+def image_endpoint(prompt: str = Query(..., description="Описание изображения")):
+    result = client.models.generate_images(
+        model=model_image,
+        prompt=prompt,
+        config=dict(
+            number_of_images=1,
+            output_mime_type="image/jpeg",
+            person_generation="ALLOW_ALL",
+            aspect_ratio="1:1",
+            image_size="1K",
+        ),
+    )
+
+    if not result.generated_images:
+        print("No images generated.")
+        return
+
+    if len(result.generated_images) != 1:
+        print("Number of images generated does not match the requested number.")
+
+    image_bytes = result.generated_images[0].image
+    return Response(content=image_bytes, media_type="image/jpeg")
+
 
 @app.get("/")
 def read_root():
